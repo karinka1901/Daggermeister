79741a42f109b4ef7bf14a5b05552db91e00a8ba
karinka1901 <karindomagalska@gmail.com>
2023-11-05 01:08:42 +0200

    new

---
+++ Assets/Scripts/Dagger.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Dagger : MonoBehaviour
+{
+    [SerializeField] private float daggerSpeed;
+    private bool hit;
+    private float direction;
+    private BoxCollider2D boxCollider;
+    private Animator anim;
+    public bool deactivated;
+    [SerializeField]private PlayerAttack playerAttack;
+
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+        //playerAttack = GetComponent<PlayerAttack>();
+    }
+
+    private void Update()
+    {
+        if (hit) return;
+        float movementSpeed = daggerSpeed * Time.deltaTime * direction;
+        transform.Translate(movementSpeed, 0, 0);
+
+        if (!playerAttack.activeDagger)
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hit = true;
+        boxCollider.enabled = false;
+        anim.SetTrigger("Explode");
+    }
+
+    public void SetDirection(float _direction)
+    {
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        boxCollider.enabled = true;
+
+        float localScaleX = transform.localScale.x;
+        if (Mathf.Sign(localScaleX) != _direction)
+            localScaleX = -localScaleX;
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+
+    }
+
+    private void Deactivate() //event for the animation
+    {
+        gameObject.SetActive(false);
+        playerAttack.activeDagger = false;
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,89 @@
+
+using System.Collections;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private PlayerMove playerMovement;
+    [SerializeField] private GameObject dagger;
+    [SerializeField] public Transform shootingPoint;
+    public bool activeDagger;
+    public Animator playerAnim;
+    [SerializeField] private Animator daggerAnim;
+    [SerializeField] private bool isTeleporting;
+
+    private void Awake()
+    {
+        playerAnim = GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+
+       
+        if (Input.GetButtonDown("Fire1"))
+
+        {
+            if (isTeleporting)
+            {
+                EndTeleportation();
+            }
+
+            if (activeDagger)
+            {
+                TeleportToDagger();
+
+            }
+            else
+            {
+                playerAnim.SetTrigger("Throw");
+
+
+            }
+
+            
+        }
+    }
+
+    private void StartAttack() //animation event
+    {
+        Attack();
+    }
+
+    private void Attack()
+    {
+        dagger.transform.position = shootingPoint.position;
+        dagger.GetComponent<Dagger>().SetDirection(Mathf.Sign(transform.localScale.x));
+        activeDagger = true; 
+        StartCoroutine(Explode(0.3f));
+    }
+
+    private IEnumerator Explode(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        daggerAnim.SetTrigger("Poof");
+
+    }
+
+    private void TeleportToDagger()
+    {
+        if (activeDagger)
+        {
+            transform.position = dagger.transform.position;
+            playerAnim.SetTrigger("teleport");
+            activeDagger = false;
+            isTeleporting = true;
+        }
+    }
+
+    private void EndTeleportation()
+    { 
+        playerAnim.ResetTrigger("teleport");
+        isTeleporting = false;
+    }
+}
+
+
+
+
